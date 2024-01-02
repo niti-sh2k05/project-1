784f078d6519698c0d162523b2fe1267d57e974e
NITISH T <153260838+niti-sh2k05@users.noreply.github.com>
2024-01-02 16:25:38 +0530

    Add files via upload

---
+++ Pyramid pattern.cpp
@@ -0,0 +1,13 @@
+//patterns
+
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i=1;i<=20;i++){
+cout << endl;
+
+
+        for(int j=1;j<i;j++){
+            cout<<" *";
+        }
+    }}
